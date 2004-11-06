034a0598a4516a373a42d704e4b749b1b90d0deb
Christian Hammond <chipx86@chipx86.com>
2004-11-06 23:36:18 +0000

    Move the emacs mode string out of the Doxygen comment and into its own.

--- libnotify/notify.h
+++ libnotify/notify.h
@@ -1,4 +1,5 @@
-/** -*- mode: c-mode; tab-width: 4; indent-tabs-mode: t; -*-
+/* -*- mode: c-mode; tab-width: 4; indent-tabs-mode: t; -*- */
+/**
  * @file libnotify/notify.h Notifications library
  *
  * @Copyright (C) 2004 Christian Hammond
