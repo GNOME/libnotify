ea9f0bc684b3ee909c33291a6290b226c5f4007b
Christian Hammond <chipx86@chipx86.com>
2006-01-29 21:17:56 +0000

    Make Doxygen happy.

--- libnotify/notify.h
+++ libnotify/notify.h
@@ -74,10 +74,10 @@ GList *notify_get_server_caps(void);
  *
  * The strings returned must be freed.
  *
- * @param ret_name     The returned product name of the server.
- * @param ret_vendor   The returned vendor.
- * @param ret_version  The returned server version.
- * @param ret_spec_ver The returned specification version supported.
+ * @param ret_name         The returned product name of the server.
+ * @param ret_vendor       The returned vendor.
+ * @param ret_version      The returned server version.
+ * @param ret_spec_version The returned specification version supported.
  *
  * @return TRUE if the call succeeded, or FALSE if there were errors.
  */
