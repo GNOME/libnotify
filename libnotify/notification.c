0ab47f074648bb00cb7eb8b68d3d15aa63c9777b
Maximiliano Sandoval R <msandova@gnome.org>
2023-10-06 17:30:41 +0200

    notification: Use g_signal_connect_object
    This ensures the notification lives as long as the signal. This was
    added in 2.62 so we add that to meson.build.
    
    Fixes https://gitlab.gnome.org/GNOME/libnotify/-/issues/34

--- libnotify/notification.c
+++ libnotify/notification.c
@@ -405,8 +405,8 @@ notify_notification_dispose (GObject *object)
         }
 
         proxy = _notify_get_proxy (NULL);
-        if (proxy != NULL && priv->proxy_signal_handler != 0) {
-                g_signal_handler_disconnect (proxy, priv->proxy_signal_handler);
+        if (proxy != NULL) {
+                g_clear_signal_handler (&priv->proxy_signal_handler, proxy);
         }
 
         g_clear_object (&priv->icon_pixbuf);
@@ -684,19 +684,11 @@ activate_action (NotifyNotification *notification,
                 return FALSE;
         }
 
-        /* Some clients have assumed it is safe to unref the
-         * Notification at the end of their NotifyActionCallback
-         * so we add a temporary ref until we're done with it.
-         */
-        g_object_ref (notification);
-
         notification->priv->activating = TRUE;
         pair->cb (notification, (char *) action, pair->user_data);
         notification->priv->activating = FALSE;
         g_free (notification->priv->activation_token);
         notification->priv->activation_token = NULL;
-
-        g_object_unref (notification);
 
         return TRUE;
 }
@@ -1107,10 +1099,11 @@ notify_notification_show (NotifyNotification *notification,
         }
 
         if (priv->proxy_signal_handler == 0) {
-                priv->proxy_signal_handler = g_signal_connect (proxy,
-                                                               "g-signal",
-                                                               G_CALLBACK (proxy_g_signal_cb),
-                                                               notification);
+                priv->proxy_signal_handler = g_signal_connect_object (proxy,
+                                                                      "g-signal",
+                                                                      G_CALLBACK (proxy_g_signal_cb),
+                                                                      notification,
+                                                                      0);
         }
 
         if (_notify_uses_portal_notifications ()) {
