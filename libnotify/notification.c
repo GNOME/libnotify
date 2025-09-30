GNOME/libnotify

commit ea5c23c03a0d292f971d630ab3775c4fced847db

notification: Allow using a desktop file as is from a snap if readable
Some snaps may have the power to open desktop files and run their
binaries, so in such case let's just them allow to use the desktop file

libnotify/notification.c
@@ -24,6 +24,7 @@
 #include "config.h"
 
 #include <gio/gio.h>
+#include <gio/gdesktopappinfo.h>
 
 #include "notify.h"
 #include "internal.h"
@@ -573,6 +574,16 @@ try_prepend_snap_desktop (NotifyNotification *notification,
 
         if (ret == NULL && _notify_get_snap_name () != NULL &&
             strchr (desktop, G_DIR_SEPARATOR) == NULL) {
+                g_autoptr(GDesktopAppInfo) app_info = NULL;
+                g_autofree char *desktop_id = NULL;
+
+                desktop_id = g_strconcat (desktop, ".desktop", NULL);
+                if ((app_info = g_desktop_app_info_new (desktop_id))) {
+                        /* Just return the desktop file if it's usable. */
+                        g_clear_object (&app_info);
+                        return NULL;
+                }
+
                 ret = g_strdup_printf ("%s_%s", _notify_get_snap_name (), desktop);
         }
 

meson.build
@@ -44,9 +44,10 @@ glib_req_version = '>= 2.62.0'
 gdk_pixbuf_dep = dependency('gdk-pixbuf-2.0')
 glib_dep = dependency('glib-2.0', version: glib_req_version)
 gio_dep = dependency('gio-2.0', version: glib_req_version)
+gio_unix_dep = dependency('gio-unix-2.0', version: glib_req_version)
 gtk_dep = dependency('gtk4', version: '>= 4.0', required: get_option('tests'))
 
-libnotify_deps = [gdk_pixbuf_dep, gio_dep, glib_dep]
+libnotify_deps = [gdk_pixbuf_dep, gio_dep, glib_dep, gio_unix_dep]
 tests_deps = [gtk_dep]
 
 conf = configuration_data()

commit 13de65ad2a76255ffde5d6da91d246cd7226583b

build: Do not use GDesktopAppInfo if not available
It's not available in macos, and apparently libnotify is compiled there
so let's support the platform

Fixes: ea5c23c03a0d292f971d630ab3775c4fced847db
Closes: #62

config.h.meson
@@ -2,3 +2,6 @@
 
 /* Version number of package */
 #mesondefine VERSION
+
+/* Headers availability */
+#mesondefine HAVE_GIO_DESKTOP_APP_INFO

libnotify/notification.c
@@ -24,7 +24,10 @@
 #include "config.h"
 
 #include <gio/gio.h>
+
+#ifdef HAVE_GIO_DESKTOP_APP_INFO
 #include <gio/gdesktopappinfo.h>
+#endif
 
 #include "notify.h"
 #include "internal.h"
@@ -575,6 +578,7 @@ try_prepend_snap_desktop (NotifyNotification *notification,
 
         if (ret == NULL && _notify_get_snap_name () != NULL &&
             strchr (desktop, G_DIR_SEPARATOR) == NULL) {
+#ifdef HAVE_GIO_DESKTOP_APP_INFO
                 g_autoptr(GDesktopAppInfo) app_info = NULL;
                 g_autofree char *desktop_id = NULL;
 
@@ -584,6 +588,7 @@ try_prepend_snap_desktop (NotifyNotification *notification,
                         g_clear_object (&app_info);
                         return NULL;
                 }
+#endif /* HAVE_GIO_DESKTOP_APP_INFO */
 
                 ret = g_strdup_printf ("%s_%s", _notify_get_snap_name (), desktop);
         }

meson.build
@@ -49,11 +49,17 @@ gio_dep = dependency('gio-2.0', version: glib_req_version)
 gio_unix_dep = dependency('gio-unix-2.0', version: glib_req_version)
 gtk_dep = dependency('gtk4', version: '>= 4.0', required: get_option('tests'))
 
+have_gio_desktop_app_info = cc.check_header('gio/gdesktopappinfo.h',
+  dependencies: gio_unix_dep,
+  required: host_machine.system() == 'linux',
+)
+
 libnotify_deps = [gdk_pixbuf_dep, gio_dep, glib_dep, gio_unix_dep]
 tests_deps = [gtk_dep]
 
 conf = configuration_data()
 conf.set_quoted('VERSION', meson.project_version())
+conf.set('HAVE_GIO_DESKTOP_APP_INFO', have_gio_desktop_app_info)
 configure_file(input: 'config.h.meson',
   output : 'config.h',
   configuration : conf)